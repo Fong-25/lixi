1aa4d626c0e25603927c7a27d430f3de117cf962
Fong <phamthanhphong160607@gmail.com>
2025-01-28 20:04:05 +0700

    fw

--- frontend/index.js
+++ frontend/index.js
@@ -22,6 +22,14 @@ envelope.addEventListener('click', async function(e) {
         greeting.textContent = `${list2[index2]}`;
         amount.textContent = `Mức lì xì: ${lucky}VNĐ`;
         envelope.classList.toggle('flap');
+        confetti({
+            particleCount: 150,
+            startVelocity: 30,
+            spread: 360,
+            decay:0.9,
+            ticks: 4000
+            }
+        );
         // Wait for the animation to finish
         await new Promise(resolve => setTimeout(resolve, 4200));
         // Send data to backend        
